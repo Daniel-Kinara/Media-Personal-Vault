0d4dec9971e7dab447e0cfa993038b5d0fe67597
Daniel Kinara <danielosoro05@gmail.com>
2026-01-08 14:59:57 +0300

    error 404

--- media-vault/vite.config.js
+++ media-vault/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // ← CHANGE TO THIS (dot-slash)
+  base: '/media-personal-vault/',
 })
