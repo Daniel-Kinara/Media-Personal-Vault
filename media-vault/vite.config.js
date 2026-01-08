0ce331846ee51fdcc4f724388dad084cdcd4b365
Daniel Kinara <danielosoro05@gmail.com>
2026-01-08 14:28:13 +0300

    changes made

--- media-vault/vite.config.js
+++ media-vault/vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const repoName = 'media-personal-vault'
 export default defineConfig({
   plugins: [react()],
-  base: '/media-personal-vault/',
+  base: `/${repoName}/`,
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
